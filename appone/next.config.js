fab70baca0e7b3ab4838781ce3fb283e7b9e3b0e
Ravi Parmar <parmarravi950@gmail.com>
2022-04-26 18:27:59 +0530

    first commit

---
+++ appone/next.config.js
@@ -0,0 +1,24 @@
+const withTM = require("next-transpile-modules")(["shared"]);
+const withPWA = require("next-pwa");
+
+module.exports = withTM([withPWA], {
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: false,
+    fallbacks: {
+      image: "/static/images/fallback.png",
+    },
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf)$/,
+      use: {
+        loader: "url-loader",
+        options: 100000,
+        name: "[name].[ext]",
+      },
+    });
+    return config;
+  },
+});
---
+++ apptwo/server.js
@@ -0,0 +1,27 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 8000;
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    if (
+      pathname === "/sw.js" ||
+      /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)
+    ) {
+      const filePath = join(__dirname, ".next", pathname);
+      app.serveStatic(req, res, filePath);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
+});
